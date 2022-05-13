a5944366bdf52fc74026c4039c13a890ae175b7b
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-13 10:29:22 -0700

    2nd test for Order written

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -14,5 +14,15 @@ namespace VendorOrder.Tests
       Order newOrder = new Order();
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void OrderConstructor_ReturnsInstanceOfDescription_Order()
+    {
+      string description = "coffee cups";
+      double price = 10.50;
+      string date = 5/13/2022;
+      Order newOrder = new Order (description, price, date);
+      string result = newOrder.Description;
+      Assert.AreEqual(description, result);
+    }
   }
 }
--- VendorOrder/Models/Order.cs
+++ VendorOrder/Models/Order.cs
@@ -4,6 +4,11 @@ namespace VendorOrder.Models
 {
   public class Order
   {
-
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; set; }
+    private static List<Order> _instances = new List<Order> {};
+   
   }
 }
