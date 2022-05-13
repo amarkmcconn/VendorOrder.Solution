69abecd6b69736e90c4ec978b3dfda2a00cee0b1
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-13 09:19:31 -0700

    2nd test for Vendor written.

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -6,17 +6,25 @@ using VendorOrder.Models;
 namespace VendorOrder.Tests
 {
   [TestClass]
-  public class VendorTests  
-  {
-    // public void Dipose()
-    // {
-    //   Vendor.ClearAll();
-    // }
+  public class VendorTests : IDisposable
+  {  
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor();
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsNameOfVendor_String()
+    {
+      string name = "Home Depot";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
     }
   }
 
--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -4,6 +4,21 @@ namespace VendorOrder.Models
 {
   public class Vendor
   {
-    
+    public string Name { get; set; }
+    public int Id { get; set; }
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public List<Order> Orders { get; set; }
+    public Vendor (string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
   }
 }
