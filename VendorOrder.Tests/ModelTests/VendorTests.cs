a4c55c87d435e721c59d1cbdf343590c9a833791
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-13 09:04:27 -0700

    1st test for Vendor written = good pass.

---
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using VendorOrder.Models;
+
+namespace VendorOrder.Tests
+{
+  [TestClass]
+  public class VendorTests  
+  {
+    // public void Dipose()
+    // {
+    //   Vendor.ClearAll();
+    // }
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor();
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+
+}
